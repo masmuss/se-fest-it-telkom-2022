13721d5221e1112844108c0a66e6a83cdf715b10
dima5fahrul <dfpa.05@gmail.com>
2022-03-22 23:56:46 +0700

    persentase detail html

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -4,13 +4,11 @@ const sumenepPieChart = new Chart(sumenepPie, {
 	type: "doughnut",
 	data: {
 		labels: ["Ikan bandeng", "Udang", "Rumput laut", "Ikan campuran"],
-		datasets: [
-			{
-				label: "Hasil laut",
-				data: [98, 2, 1, 1],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		datasets: [{
+			text: "Hasil laut",
+			data: [94, 2, 1, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -28,13 +26,11 @@ const sumenepBarChart = new Chart(sumenepBar, {
 	type: "bar",
 	data: {
 		labels: [670400, 2300, 448, 233],
-		datasets: [
-			{
-				label: "Hasil laut",
-				data: [670400, 2300, 448, 233],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		datasets: [{
+			text: "Hasil laut",
+			data: [94, 2, 2, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -71,13 +67,11 @@ const gresikPieChart = new Chart(gresikPie, {
 	type: "doughnut",
 	data: {
 		labels: ["Ikan bandeng", "Ikan campuran", "Udang", "Ikan kerapu"],
-		datasets: [
-			{
-				label: "Hasil laut",
-				data: [70, 19, 10, 1],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		datasets: [{
+			label: "Hasil laut",
+			data: [70, 19, 10, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -94,14 +88,12 @@ const gresikBar = document.getElementById("gresik-bar").getContext("2d");
 const gresikBarChart = new Chart(gresikBar, {
 	type: "bar",
 	data: {
-		labels: [94.0, 25.0, 13.2, 131],
-		datasets: [
-			{
-				label: "Hasil laut",
-				data: [94.0, 25.0, 13.2, 131],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		labels: [94000, 25600, 13200, 131],
+		datasets: [{
+			label: "Hasil laut",
+			data: [70, 19, 10, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -138,13 +130,11 @@ const ndarjoPieChart = new Chart(ndarjoPie, {
 	type: "doughnut",
 	data: {
 		labels: ["Ikan bandeng", "Udang", "Rumput laut", "Ikan campuran"],
-		datasets: [
-			{
-				label: "Hasil laut",
-				data: [56, 20, 16, 7],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		datasets: [{
+			label: "Hasil laut",
+			data: [56, 20, 16, 7],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -162,13 +152,11 @@ const ndarjoBarChart = new Chart(ndarjoBar, {
 	type: "bar",
 	data: {
 		labels: [34200, 12400, 10100, 4000],
-		datasets: [
-			{
-				label: "Hasil laut",
-				data: [34200, 12400, 10100, 4000],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		datasets: [{
+			label: "Hasil laut",
+			data: [56, 20, 16, 7],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -204,14 +192,12 @@ const lamonganPie = document.getElementById("lamongan-pie").getContext("2d");
 const lamonganPieChart = new Chart(lamonganPie, {
 	type: "doughnut",
 	data: {
-		labels: ["Ikan bandeng", "Ikan kerapu", "Udang", "Rumput laut"],
-		datasets: [
-			{
-				label: "Hasil laut (dalam persen)",
-				data: [58, 2, 39, 1],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		labels: ["Ikan bandeng", "Udang", "Ikan Kerapu", "Rumput laut"],
+		datasets: [{
+			label: "Hasil laut (dalam persen)",
+			data: [58, 39, 2, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -228,14 +214,12 @@ const lamonganBar = document.getElementById("lamongan-bar").getContext("2d");
 const lamonganBarChart = new Chart(lamonganBar, {
 	type: "bar",
 	data: {
-		labels: [24.000, 1.000, 15.900, 8],
-		datasets: [
-			{
-				label: "Hasil laut (Ton)",
-				data: [24.000, 1.000, 15.900, 8],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		labels: [24000, 1000, 15900, 8],
+		datasets: [{
+			label: "Hasil laut (Ton)",
+			data: [58, 39, 2, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -271,14 +255,12 @@ const banyuwangiPie = document.getElementById("banyuwangi-pie").getContext("2d")
 const banyuwangiPieChart = new Chart(banyuwangiPie, {
 	type: "doughnut",
 	data: {
-		labels: ["Udang", "Rumput laut", "Ikan bandeng", "Ikan campuran"],
-		datasets: [
-			{
-				label: "Hasil laut (dalam persen)",
-				data: [86, 10, 1, 2],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		labels: ["Udang", "Rumput laut", "Ikan Campuran", "Ikan Bandeng"],
+		datasets: [{
+			label: "Hasil laut (dalam persen)",
+			data: [86, 10, 2, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
@@ -295,14 +277,12 @@ const banyuwangiBar = document.getElementById("banyuwangi-bar").getContext("2d")
 const banyuwangiBarChart = new Chart(banyuwangiBar, {
 	type: "bar",
 	data: {
-		labels: [21.500, 2.700, 124, 500],
-		datasets: [
-			{
-				label: "Hasil laut (Ton)",
-				data: [21.500, 2.700, 124, 500],
-				backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
-			},
-		],
+		labels: [21500, 2700, 124, 500],
+		datasets: [{
+			label: "Hasil laut (Ton)",
+			data: [86, 10, 2, 1],
+			backgroundColor: ["rgba(241, 87, 33, 0.8)", "rgba(54, 162, 235, 0.8)", "rgba(255, 206, 86, 0.8)", "rgba(75, 192, 192, 0.8)"],
+		}, ],
 	},
 	options: {
 		responsive: true,
